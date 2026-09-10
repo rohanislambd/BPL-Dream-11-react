58b49203ee14ecd436dbe13a730fe044e20e8169
rohanislambd <coderootbdofficial24@gmail.com>
2026-09-10 20:38:55 +0600

    setup react with tailwind css and daisyui

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+        <h2 className="text-5xl text-red-800">BPL dream 11</h2>
+        <button className="btn btn-success">Success</button>
+
+    </>
+  )
+}
+
+export default App
